247631dd2ad1b817496f7bce0a4aec5de63266e7
Sanjaycsk18 <sanjaycsk2004@gmail.com>
2023-03-21 17:45:20 +0530

    Var deleted

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 
-def hello(name):
+def hello():
     name = input('Enter ur name:')
     if name == "hari":
         print("Hi " + name )
     else:
         print("Hi")
 print("Hi,Ambi")
-hello(name)
+hello()
